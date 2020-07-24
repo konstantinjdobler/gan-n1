17b5b4e0103b0da332fcab0eb73cdebe9abc76a9
Dobler <konstantin.j.dobler@gmail.com>
2020-07-24 13:19:53 +0200

    clean

--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
 # Rating images according to their "fakeness" or lack thereof is an entire subdomain
 # For reasons of time, we default to visual testing by humans
+# For example, Inception score could be used to automatically garde image synthesis quality
 # Reference on how one could do it: https://github.com/dmaniry/deepIQA
